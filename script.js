ae019e29794ece77745c246333f58c947f3af67a
renatorodriguess <renato072002@gmail.com>
2023-02-03 21:50:29 -0300

    oi

---
+++ script.js
@@ -0,0 +1,10 @@
+class person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+let p1 = new person = ("João", "Silva")
+let p2 = new person = ("Henrique", "Macedo")
+let p3 = new person = ("Julia", "Leite")
